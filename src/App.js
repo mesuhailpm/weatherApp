c7466dedb0ba8ee294d21e9ac2792a11329468d5
suhailpm <tosuhailpm@gmail.com>
2023-06-12 23:42:04 +0530

    imporevemnts

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './App.css'
 
 export default function App () {
     const [city,setCity] = useState('')
-    const [keyWord,setkeyword] = useState('')
+    const [keyWord,setkeyword] = useState('New york')
     const [weatherData,setWeatherData] = useState({})
 
     console.log(weatherData)
 
     const handleKeyPress = (e) =>{
-        if(e.key === 'Enter') setkeyword(e.target.value)
+        if(e.key === 'Enter') if (e.target.value.length){setkeyword(e.target.value)}
     }
 
             useEffect(() => {
@@ -27,9 +27,39 @@ export default function App () {
 
         return(
         <div className='App'>
-            <label>Type the city</label>
-            <input onChange={(e)=>setCity(e.target.value)} value={city} onKeyPress={handleKeyPress}></input>
-            <p>{weatherData?.location?.name} </p>
+            <input onChange={(e)=>setCity(e.target.value)} value={city} placeholder='Search...' onKeyPress={handleKeyPress}></input>
+            <div className="weather">
+                <div className="left">
+                    <h3>{weatherData?.location?.name}</h3>
+                    <h4>{weatherData?.location?.country}</h4>
+                    <div className="temperature">
+                        <h1 className="temperature--c">{weatherData?.current?.temp_c}<sup>&deg;C</sup></h1>
+                        {/* <h5 className="temperature--f">{weatherData?.current?.temp_f}<sup>&deg;f</sup></h5> */}
+                        <h4 className="temperature--f">Feels like {weatherData?.current?.feelslike_c}<sup>&deg;c</sup></h4>
+                    </div>
+                </div>
+
+                <div className="right">
+                    <img src={weatherData?.current?.condition.icon}></img>
+                    <p> {weatherData?.current?.condition.text} </p>
+
+                    <div className="minmax">
+                        <h5>High: {weatherData?.forecast?.forecastday[0].day.maxtemp_c}</h5>
+                        <h5>Low: {weatherData?.forecast?.forecastday[0].day.mintemp_c}</h5>
+                    </div>
+                </div>
+
+            </div>
+            <div className="forecast">
+                {weatherData?.forecast?.forecastday[0].hour.map((hour,index)=>(
+                    <div key={index} className={`card ${!hour.is_day ? 'dark':''}`}>
+                        <p>{hour.time.split(' ')[1]}</p>
+                        {hour.dewpoint_c} &deg;c
+                        <img src={hour.condition.icon} />
+                    </div>
+                ))}
+
+            </div>
 
 
         </div>
--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,8 @@ const key  = 'dfaa888e786c46b69e385936231206'
 const URL = 'http://api.weatherapi.com/v1'
 export const fetchWeatherBySearch = async (query) =>{
     try {
-        const response = await axios.get(`${URL}/current.json?key=${key}&q=${query}`)
+        const response = await axios.get(`${URL}/forecast.json?key=${key}&q=${query}`)
+        console.log(response)
         return response.data
         
     } catch (error) {
